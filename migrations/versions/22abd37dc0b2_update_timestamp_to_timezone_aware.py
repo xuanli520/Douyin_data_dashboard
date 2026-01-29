144091d94c473eb2770d9c18086beab47b995f14
17882209859 <2990668364@qq.com>
2026-01-29 20:26:45 +0800

    fix: database conflict

--- migrations/versions/22abd37dc0b2_update_timestamp_to_timezone_aware.py
+++ migrations/versions/22abd37dc0b2_update_timestamp_to_timezone_aware.py
@@ -14,7 +14,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = "22abd37dc0b2"
-down_revision: Union[str, Sequence[str], None] = "9f5f1dfe3a30"
+down_revision: Union[str, Sequence[str], None] = "fc7a2b1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
